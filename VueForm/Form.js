ce735c013f5610ff22cc76e58a33d195ade04a2a
Anton Kuznetsov <isnifer@gmail.com>
2018-11-07 14:34:40 +0300

    [1.0.3] Check if handleReset exists

--- VueForm/Form.js
+++ VueForm/Form.js
@@ -1,11 +1,14 @@
 import { isEmpty, isNil } from 'lodash'
 import { Form } from 'element-ui'
+import invariant from 'invariant'
 import { withHooks, useFormState } from '../hooks'
 
 export default withHooks((h, props, instance) => {
   // eslint-disable-next-line
   const { _state: state, _errors: errors } = instance.$data
   const { initialValues = {}, handleSubmit, handleDisabled, handleReset } = props
+
+  invariant(handleSubmit, 'Prop "handleSubmit" is required')
 
   instance.$registerFormComponent = (name, initialValue, error) => {
     const formComponentInitialValue = !isNil(initialValue) ? initialValue : initialValues[name]
@@ -35,7 +38,9 @@ export default withHooks((h, props, instance) => {
       instance.$set(errors, key, null)
     })
 
-    return handleReset(initialValues)
+    if (handleReset) {
+      handleReset(initialValues)
+    }
   }
 
   return (
